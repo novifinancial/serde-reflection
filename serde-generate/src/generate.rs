f2173a1d64d94615c68c0d0aa99c418c3736ac6a
Mathieu Baudet <mathieubaudet@calibra.com>
2020-04-28 19:12:27 -0700

    [serde generate] add support for C++

--- serde-generate/src/generate.rs
+++ serde-generate/src/generate.rs
@@ -7,7 +7,7 @@
 //! cargo run -p serde-generate -- --help
 //! '''
 
-use serde_generate::{python3, rust};
+use serde_generate::{cpp, python3, rust};
 use serde_reflection::Registry;
 use std::path::PathBuf;
 use structopt::{clap::arg_enum, StructOpt};
@@ -16,6 +16,7 @@ arg_enum! {
 #[derive(Debug, StructOpt)]
 enum Language {
     Python3,
+    Cpp,
     Rust,
 }
 }
@@ -43,6 +44,7 @@ fn main() {
 
     match options.language {
         Language::Python3 => python3::output(&mut out, &registry).unwrap(),
+        Language::Cpp => cpp::output(&mut out, &registry).unwrap(),
         Language::Rust => rust::output(&mut out, /* with_derive_macros */ true, &registry).unwrap(),
     }
 }
