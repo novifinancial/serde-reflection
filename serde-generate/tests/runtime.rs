df0fd1dade9e05058ffcc5b3a14ed3af0ca6b44b
Mathieu Baudet <mathieubaudet@calibra.com>
2020-05-28 18:27:12 -0700

    [serde generate] e2e runtime test for Rust

--- serde-generate/tests/runtime.rs
+++ serde-generate/tests/runtime.rs
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
 use serde::{Deserialize, Serialize};
-use serde_generate::python3;
+use serde_generate::{python3, rust};
 use serde_reflection::{Registry, Result, Samples, Tracer, TracerConfig};
 use std::fs::File;
 use std::io::Write;
@@ -130,5 +130,72 @@ for encoding in encodings:
     std::io::stdout().write(&output.stdout).unwrap();
     std::io::stderr().write(&output.stderr).unwrap();
     assert_eq!(String::new(), String::from_utf8_lossy(&output.stderr));
+    assert!(output.status.success());
+}
+
+// Full test using cargo. This may take a while.
+#[test]
+fn test_rust_bincode_runtime() {
+    let registry = test_utils::get_registry().unwrap();
+    let dir = tempdir().unwrap();
+    std::fs::write(
+        dir.path().join("Cargo.toml"),
+        r#"[package]
+name = "testing2"
+version = "0.1.0"
+edition = "2018"
+
+[dependencies]
+hex = "0.4.2"
+serde = { version = "1.0", features = ["derive"] }
+serde_bytes = "0.11"
+bincode = "1.2"
+
+[workspace]
+"#,
+    )
+    .unwrap();
+    std::fs::create_dir(dir.path().join("src")).unwrap();
+    let source_path = dir.path().join("src/main.rs");
+    let mut source = File::create(&source_path).unwrap();
+    rust::output(&mut source, /* with_derive_macros */ true, &registry).unwrap();
+
+    let values = test_utils::get_sample_values();
+    let hex_encodings: Vec<_> = values
+        .iter()
+        .map(|v| format!("\"{}\"", hex::encode(&bincode::serialize(&v).unwrap())))
+        .collect();
+
+    writeln!(
+        source,
+        r#"
+fn main() {{
+    let hex_encodings = vec![{}];
+
+    for hex_encoding in hex_encodings {{
+        let encoding = hex::decode(hex_encoding).unwrap();
+        let value = bincode::deserialize::<SerdeData>(&encoding).unwrap();
+
+        let s = bincode::serialize(&value).unwrap();
+        assert_eq!(s, encoding);
+    }}
+}}
+"#,
+        hex_encodings.join(", ")
+    )
+    .unwrap();
+
+    // Use a stable `target` dir to avoid downloading and recompiling crates everytime.
+    let target_dir = std::env::current_dir().unwrap().join("../target");
+    let output = Command::new("cargo")
+        .current_dir(dir.path())
+        .arg("run")
+        .arg("--target-dir")
+        .arg(target_dir)
+        .output()
+        .unwrap();
+
+    std::io::stdout().write(&output.stdout).unwrap();
+    std::io::stderr().write(&output.stderr).unwrap();
     assert!(output.status.success());
 }
