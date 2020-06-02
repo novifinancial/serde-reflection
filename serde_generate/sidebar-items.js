a4945504b11457532978a991cda901a74070d460
ci-build <ci-build@calibra.com>
2020-06-02 04:15:44 +0000

    [skip ci] documentation update

--- serde_generate/sidebar-items.js
+++ serde_generate/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["analyzer",""],["python3",""],["rust",""]]});
+initSidebarItems({"mod":[["analyzer","Dependency analysis and topological sort for Serde formats."],["python3","Support for code-generation in Python 3"],["rust","Support for code-generation in Rust"]]});
