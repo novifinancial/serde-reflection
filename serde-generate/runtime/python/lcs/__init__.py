743f7ec9848481e1d2ccee172a929695d340c3e8
Mathieu Baudet <mathieubaudet@calibra.com>
2020-06-15 18:31:05 -0700

    [serde generate] add LCS runtime for python

---
+++ serde-generate/runtime/python/lcs/__init__.py
@@ -0,0 +1,299 @@
+# Copyright (c) Facebook, Inc. and its affiliates
+# SPDX-License-Identifier: MIT OR Apache-2.0
+
+# pyre-ignore-all-errors
+
+import dataclasses
+import collections
+
+import serde_types as st
+import typing
+from typing import get_type_hints
+
+
+def decode_uleb128_as_u32(content: bytes) -> typing.Tuple[st.uint32, bytes]:
+    value = 0
+    for shift in range(0, 32, 7):
+        byte = int.from_bytes(content[0:1], "little", signed=False)
+        content = content[1:]
+        digit = byte & 0x7F
+        value |= digit << shift
+        if digit == byte:
+            if shift > 0 and digit == 0:
+                raise ValueError("Invalid uleb128 number (unexpected zero digit)")
+            break
+
+    if value >= 1 << 32:
+        raise ValueError("Overflow while parsing uleb128-encoded uint32 value")
+    return value, content
+
+
+def decode_length(content: bytes) -> typing.Tuple[int, bytes]:
+    value, content = decode_uleb128_as_u32(content)
+    if value > 1 << 31:
+        raise ValueError("Length exceeds the maximum supported value.")
+    return value, content
+
+
+def decode_variant_index(content: bytes) -> typing.Tuple[int, bytes]:
+    return decode_uleb128_as_u32(content)
+
+
+def decode_str(content: bytes) -> typing.Tuple[str, bytes]:
+    strlen, content = decode_length(content)
+    val, content = content[0:strlen].decode(), content[strlen:]
+    return val, content
+
+
+def decode_bytes(content: bytes) -> typing.Tuple[bytes, bytes]:
+    len, content = decode_length(content)
+    val, content = content[:len], content[len:]
+    return val, content
+
+
+def encode_u32_as_uleb128(value: int) -> bytes:
+    res = b""
+    while value >= 0x80:
+        res += ((value & 0x7F) | 0x80).to_bytes(1, "little", signed=False)
+        value >>= 7
+    res += value.to_bytes(1, "little", signed=False)
+    return res
+
+
+def encode_length(value: int) -> bytes:
+    return encode_u32_as_uleb128(value)
+
+
+def encode_variant_index(value: int) -> bytes:
+    return encode_u32_as_uleb128(value)
+
+
+def encode_str(value: str) -> bytes:
+    return encode_length(len(value)) + value.encode()
+
+
+def encode_bytes(value: bytes) -> bytes:
+    return encode_length(len(value)) + value
+
+
+def not_implemented():
+    raise NotImplementedError
+
+
+primitive_encode_map = {
+    st.bool: lambda x: int(x).to_bytes(1, "little", signed=False),
+    st.uint8: lambda x: int(x).to_bytes(1, "little", signed=False),
+    st.uint16: lambda x: int(x).to_bytes(2, "little", signed=False),
+    st.uint32: lambda x: int(x).to_bytes(4, "little", signed=False),
+    st.uint64: lambda x: int(x).to_bytes(8, "little", signed=False),
+    st.uint128: lambda x: int(x).to_bytes(16, "little", signed=False),
+    st.int8: lambda x: int(x).to_bytes(1, "little", signed=True),
+    st.int16: lambda x: int(x).to_bytes(2, "little", signed=True),
+    st.int32: lambda x: int(x).to_bytes(4, "little", signed=True),
+    st.int64: lambda x: int(x).to_bytes(8, "little", signed=True),
+    st.int128: lambda x: int(x).to_bytes(16, "little", signed=True),
+    st.float32: lambda x: not_implemented(),
+    st.float64: lambda x: not_implemented(),
+    st.unit: lambda x: b"",
+    st.char: lambda x: not_implemented(),
+    str: lambda x: encode_str(x),
+    bytes: lambda x: encode_bytes(x),
+}
+
+primitive_decode_map = {
+    st.bool: lambda content: (
+        st.bool(int.from_bytes(content[:1], byteorder="little", signed=False)),
+        content[1:],
+    ),
+    st.uint8: lambda content: (
+        st.uint8(int.from_bytes(content[:1], byteorder="little", signed=False)),
+        content[1:],
+    ),
+    st.uint16: lambda content: (
+        st.uint16(int.from_bytes(content[:2], byteorder="little", signed=False)),
+        content[2:],
+    ),
+    st.uint32: lambda content: (
+        st.uint32(int.from_bytes(content[:4], byteorder="little", signed=False)),
+        content[4:],
+    ),
+    st.uint64: lambda content: (
+        st.uint64(int.from_bytes(content[:8], byteorder="little", signed=False)),
+        content[8:],
+    ),
+    st.uint128: lambda content: (
+        st.uint128(int.from_bytes(content[:16], byteorder="little", signed=False)),
+        content[16:],
+    ),
+    st.int8: lambda content: (
+        st.int8(int.from_bytes(content[:1], byteorder="little", signed=True)),
+        content[1:],
+    ),
+    st.int16: lambda content: (
+        st.int16(int.from_bytes(content[:2], byteorder="little", signed=True)),
+        content[2:],
+    ),
+    st.int32: lambda content: (
+        st.int32(int.from_bytes(content[:4], byteorder="little", signed=True)),
+        content[4:],
+    ),
+    st.int64: lambda content: (
+        st.int64(int.from_bytes(content[:8], byteorder="little", signed=True)),
+        content[8:],
+    ),
+    st.int128: lambda content: (
+        st.int128(int.from_bytes(content[:16], byteorder="little", signed=True)),
+        content[16:],
+    ),
+    st.float32: lambda content: not_implemented(),
+    st.float64: lambda content: not_implemented(),
+    st.unit: lambda content: (None, content),
+    st.char: lambda content: not_implemented(),
+    str: lambda content: decode_str(content),
+    bytes: lambda content: decode_bytes(content),
+}
+
+# noqa: C901
+def serialize(obj: typing.Any, obj_type) -> bytes:
+    result = b""
+
+    if obj_type in primitive_encode_map:
+        result += primitive_encode_map[obj_type](obj)
+
+    elif hasattr(obj_type, "__origin__"):  # Generic type
+        types = getattr(obj_type, "__args__")
+
+        if getattr(obj_type, "__origin__") == collections.abc.Sequence:  # Sequence
+            assert len(types) == 1
+            item_type = types[0]
+            result += encode_length(len(obj))
+            result += b"".join([serialize(item, item_type) for item in obj])
+
+        elif getattr(obj_type, "__origin__") == tuple:  # Tuple
+            for i in range(len(obj)):
+                result += serialize(obj[i], types[i])
+
+        elif getattr(obj_type, "__origin__") == typing.Union:  # Option
+            assert len(types) == 2 and types[1] == type(None)
+            if obj is None:
+                result += b"\x00"
+            else:
+                result += b"\x01"
+                result += serialize(obj, types[0])
+
+        elif getattr(obj_type, "__origin__") == dict:  # Map
+            assert len(types) == 2
+            item_type = typing.Tuple[types[0], types[1]]
+            result += encode_length(len(obj))
+            # Sorting by lexicographic order on the serialized item (or equivalently, serialized keys).
+            serialized_items = sorted(
+                serialize(item, item_type) for item in obj.items()
+            )
+            for s in serialized_items:
+                result += s
+
+        else:
+            raise ValueError("Unexpected type", obj_type)
+
+    else:
+        if not isinstance(obj, obj_type):
+            raise ValueError("Wrong Value for the type", obj, obj_type)
+
+        if hasattr(obj.__class__, "INDEX"):  # Variant
+            result += encode_variant_index(obj.__class__.INDEX)
+
+        # Content of struct or variant
+        typing_hints = get_type_hints(obj.__class__)
+        for (key, val) in obj.__dict__.items():
+            if key.startswith("__"):
+                continue
+            result += serialize(val, typing_hints[key])
+
+    return result
+
+
+# noqa
+def deserialize(content: bytes, obj_type):
+    if obj_type in primitive_decode_map:
+        res, content = primitive_decode_map[obj_type](content)
+        return res, content
+
+    elif hasattr(obj_type, "__origin__"):  # Generic type
+        types = getattr(obj_type, "__args__")
+        if getattr(obj_type, "__origin__") == collections.abc.Sequence:  # Sequence
+            assert len(types) == 1
+            item_type = types[0]
+            seqlen, content = decode_length(content)
+            res = []
+            for i in range(0, seqlen):
+                item, content = deserialize(content, item_type)
+                res.append(item)
+
+            return res, content
+
+        elif getattr(obj_type, "__origin__") == tuple:  # Tuple
+            res = []
+            for i in range(len(types)):
+                item, content = deserialize(content, types[i])
+                res.append(item)
+            return tuple(res), content
+
+        elif getattr(obj_type, "__origin__") == typing.Union:  # Option
+            assert len(types) == 2 and types[1] == type(None)
+            tag = int.from_bytes(content[:1], byteorder="little", signed=False)
+            content = content[1:]
+            if tag == 0:
+                return None, content
+            elif tag == 1:
+                return deserialize(content, types[0])
+            else:
+                raise ValueError("Wrong tag for Option value")
+
+        elif getattr(obj_type, "__origin__") == dict:  # Map
+            assert len(types) == 2
+            seqlen, content = decode_length(content)
+            res = dict()
+            previous_serialized_key = None
+            for i in range(0, seqlen):
+                previous_content = content
+                key, content = deserialize(previous_content, types[0])
+                serialized_key = previous_content[: -len(content)]
+                value, content = deserialize(content, types[1])
+                if (
+                    previous_serialized_key is not None
+                    and previous_serialized_key >= serialized_key
+                ):
+                    raise ValueError(
+                        "Serialized keys in a map must be ordered by increasing lexicographic order"
+                    )
+                previous_serialized_key = serialized_key
+                res[key] = value
+
+            return res, content
+
+        else:
+            raise ValueError("Unexpected type", obj_type)
+
+    else:
+        # handle structs
+        if dataclasses.is_dataclass(obj_type):
+            values = []
+            fields = dataclasses.fields(obj_type)
+            typing_hints = get_type_hints(obj_type)
+            for field in fields:
+                field_type = typing_hints[field.name]
+                field_value, content = deserialize(content, field_type)
+                values.append(field_value)
+
+            res = obj_type(*values)
+            return res, content
+
+        # handle variant
+        elif hasattr(obj_type, "VARIANTS"):
+            variant_index, content = decode_variant_index(content)
+            new_type = obj_type.VARIANTS[variant_index]
+            res, content = deserialize(content, new_type)
+            return res, content
+
+        else:
+            raise ValueError("Unexpected type", obj_type)
--- serde-generate/tests/lcs_runtime.rs
+++ serde-generate/tests/lcs_runtime.rs
@@ -1,6 +1,7 @@
 // Copyright (c) Facebook, Inc. and its affiliates
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
+use libra_canonical_serialization as lcs;
 use serde::{Deserialize, Serialize};
 use serde_generate::{python3, rust, test_utils};
 use serde_reflection::{Registry, Result, Samples, Tracer, TracerConfig};
@@ -32,14 +33,14 @@ fn get_local_registry() -> Result<Registry> {
 }
 
 #[test]
-fn test_python_bincode_runtime_on_simple_data() {
+fn test_python_lcs_runtime_on_simple_data() {
     let registry = get_local_registry().unwrap();
     let dir = tempdir().unwrap();
     let source_path = dir.path().join("test.py");
     let mut source = File::create(&source_path).unwrap();
     python3::output(&mut source, &registry).unwrap();
 
-    let reference = bincode::serialize(&Test {
+    let reference = lcs::to_bytes(&Test {
         a: vec![4, 6],
         b: (3, 5),
         c: Choice::C { x: 7 },
@@ -48,20 +49,20 @@ fn test_python_bincode_runtime_on_simple_data() {
     writeln!(
         source,
         r#"
-import bincode
+import lcs
 
 value = Test([4, 6], (3, 5), Choice.C(7))
 
-s = bincode.serialize(value, Test)
+s = lcs.serialize(value, Test)
 assert s == bytes.fromhex("{}")
 
-v, buffer = bincode.deserialize(s, Test)
+v, buffer = lcs.deserialize(s, Test)
 assert len(buffer) == 0
 assert v == value
 assert v.c.x == 7
 
 v.b = (3, 0)
-t = bincode.serialize(v, Test)
+t = lcs.serialize(v, Test)
 assert len(t) == len(s)
 assert t != s
 "#,
@@ -83,7 +84,7 @@ assert t != s
 }
 
 #[test]
-fn test_python_bincode_runtime_on_all_supported_types() {
+fn test_python_lcs_runtime_on_all_supported_types() {
     let registry = test_utils::get_registry().unwrap();
     let dir = tempdir().unwrap();
     let source_path = dir.path().join("test.py");
@@ -93,21 +94,21 @@ fn test_python_bincode_runtime_on_all_supported_types() {
     let values = test_utils::get_sample_values();
     let hex_encodings: Vec<_> = values
         .iter()
-        .map(|v| format!("'{}'", hex::encode(&bincode::serialize(&v).unwrap())))
+        .map(|v| format!("'{}'", hex::encode(&lcs::to_bytes(&v).unwrap())))
         .collect();
 
     writeln!(
         source,
         r#"
-import bincode
+import lcs
 
 encodings = [bytes.fromhex(s) for s in [{}]]
 
 for encoding in encodings:
-    v, buffer = bincode.deserialize(encoding, SerdeData)
+    v, buffer = lcs.deserialize(encoding, SerdeData)
     assert len(buffer) == 0
 
-    s = bincode.serialize(v, SerdeData)
+    s = lcs.serialize(v, SerdeData)
     assert s == encoding
 "#,
         hex_encodings.join(", ")
@@ -132,7 +133,7 @@ for encoding in encodings:
 
 // Full test using cargo. This may take a while.
 #[test]
-fn test_rust_bincode_runtime() {
+fn test_rust_lcs_runtime() {
     let registry = test_utils::get_registry().unwrap();
     let dir = tempdir().unwrap();
     std::fs::write(
@@ -144,9 +145,9 @@ edition = "2018"
 
 [dependencies]
 hex = "0.4.2"
-serde = { version = "1.0", features = ["derive"] }
+serde = { version = "1.0.112", features = ["derive"] }
 serde_bytes = "0.11"
-bincode = "1.2"
+libra-canonical-serialization = { git = "https://github.com/libra/libra.git", branch = "testnet" }
 
 [workspace]
 "#,
@@ -160,20 +161,22 @@ bincode = "1.2"
     let values = test_utils::get_sample_values();
     let hex_encodings: Vec<_> = values
         .iter()
-        .map(|v| format!("\"{}\"", hex::encode(&bincode::serialize(&v).unwrap())))
+        .map(|v| format!("\"{}\"", hex::encode(&lcs::to_bytes(&v).unwrap())))
         .collect();
 
     writeln!(
         source,
         r#"
+use libra_canonical_serialization as lcs;
+
 fn main() {{
     let hex_encodings = vec![{}];
 
     for hex_encoding in hex_encodings {{
         let encoding = hex::decode(hex_encoding).unwrap();
-        let value = bincode::deserialize::<SerdeData>(&encoding).unwrap();
+        let value = lcs::from_bytes::<SerdeData>(&encoding).unwrap();
 
-        let s = bincode::serialize(&value).unwrap();
+        let s = lcs::to_bytes(&value).unwrap();
         assert_eq!(s, encoding);
     }}
 }}
