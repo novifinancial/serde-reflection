0cac33f89e3eab843f5d74f33e1a69da144e999b
Mathieu Baudet <mathieubaudet@calibra.com>
2020-06-23 11:35:16 -0700

    [serde generate] add missing `inline` keywords to C++ runtime

--- serde-generate/runtime/cpp/bincode.hpp
+++ serde-generate/runtime/cpp/bincode.hpp
@@ -75,40 +75,46 @@ class BincodeDeserializer {
     bool deserialize_option_tag();
 };
 
-void BincodeSerializer::serialize_str(const std::string &value) {
+inline void BincodeSerializer::serialize_str(const std::string &value) {
     serialize_len(value.size());
     for (auto c : value) {
         bytes_.push_back(c);
     }
 }
 
-void BincodeSerializer::serialize_unit() {}
+inline void BincodeSerializer::serialize_unit() {}
 
-void BincodeSerializer::serialize_f32(float) { throw "not implemented"; }
+inline void BincodeSerializer::serialize_f32(float) { throw "not implemented"; }
 
-void BincodeSerializer::serialize_f64(double) { throw "not implemented"; }
+inline void BincodeSerializer::serialize_f64(double) {
+    throw "not implemented";
+}
 
-void BincodeSerializer::serialize_char(char32_t) { throw "not implemented"; }
+inline void BincodeSerializer::serialize_char(char32_t) {
+    throw "not implemented";
+}
 
-void BincodeSerializer::serialize_bool(bool value) {
+inline void BincodeSerializer::serialize_bool(bool value) {
     bytes_.push_back((uint8_t)value);
 }
 
-void BincodeSerializer::serialize_u8(uint8_t value) { bytes_.push_back(value); }
+inline void BincodeSerializer::serialize_u8(uint8_t value) {
+    bytes_.push_back(value);
+}
 
-void BincodeSerializer::serialize_u16(uint16_t value) {
+inline void BincodeSerializer::serialize_u16(uint16_t value) {
     bytes_.push_back((uint8_t)value);
     bytes_.push_back((uint8_t)(value >> 8));
 }
 
-void BincodeSerializer::serialize_u32(uint32_t value) {
+inline void BincodeSerializer::serialize_u32(uint32_t value) {
     bytes_.push_back((uint8_t)value);
     bytes_.push_back((uint8_t)(value >> 8));
     bytes_.push_back((uint8_t)(value >> 16));
     bytes_.push_back((uint8_t)(value >> 24));
 }
 
-void BincodeSerializer::serialize_u64(uint64_t value) {
+inline void BincodeSerializer::serialize_u64(uint64_t value) {
     bytes_.push_back((uint8_t)value);
     bytes_.push_back((uint8_t)(value >> 8));
     bytes_.push_back((uint8_t)(value >> 16));
@@ -119,45 +125,45 @@ void BincodeSerializer::serialize_u64(uint64_t value) {
     bytes_.push_back((uint8_t)(value >> 56));
 }
 
-void BincodeSerializer::serialize_u128(const uint128_t &value) {
+inline void BincodeSerializer::serialize_u128(const uint128_t &value) {
     serialize_u64(value.low);
     serialize_u64(value.high);
 }
 
-void BincodeSerializer::serialize_i8(int8_t value) {
+inline void BincodeSerializer::serialize_i8(int8_t value) {
     serialize_u8((uint8_t)value);
 }
 
-void BincodeSerializer::serialize_i16(int16_t value) {
+inline void BincodeSerializer::serialize_i16(int16_t value) {
     serialize_u16((uint16_t)value);
 }
 
-void BincodeSerializer::serialize_i32(int32_t value) {
+inline void BincodeSerializer::serialize_i32(int32_t value) {
     serialize_u32((uint32_t)value);
 }
 
-void BincodeSerializer::serialize_i64(int64_t value) {
+inline void BincodeSerializer::serialize_i64(int64_t value) {
     serialize_u64((uint64_t)value);
 }
 
-void BincodeSerializer::serialize_i128(const int128_t &value) {
+inline void BincodeSerializer::serialize_i128(const int128_t &value) {
     serialize_u64(value.low);
     serialize_i64(value.high);
 }
 
-void BincodeSerializer::serialize_len(size_t value) {
+inline void BincodeSerializer::serialize_len(size_t value) {
     serialize_u64((uint64_t)value);
 }
 
-void BincodeSerializer::serialize_variant_index(size_t value) {
+inline void BincodeSerializer::serialize_variant_index(size_t value) {
     serialize_u32((uint32_t)value);
 }
 
-void BincodeSerializer::serialize_option_tag(bool value) {
+inline void BincodeSerializer::serialize_option_tag(bool value) {
     serialize_bool(value);
 }
 
-std::string BincodeDeserializer::deserialize_str() {
+inline std::string BincodeDeserializer::deserialize_str() {
     auto len = deserialize_len();
     std::string result;
     result.reserve(len);
@@ -167,28 +173,34 @@ std::string BincodeDeserializer::deserialize_str() {
     return result;
 }
 
-uint8_t BincodeDeserializer::read_byte() { return bytes_.at(pos_++); }
+inline uint8_t BincodeDeserializer::read_byte() { return bytes_.at(pos_++); }
 
-void BincodeDeserializer::deserialize_unit() {}
+inline void BincodeDeserializer::deserialize_unit() {}
 
-float BincodeDeserializer::deserialize_f32() { throw "not implemented"; }
+inline float BincodeDeserializer::deserialize_f32() { throw "not implemented"; }
 
-double BincodeDeserializer::deserialize_f64() { throw "not implemented"; }
+inline double BincodeDeserializer::deserialize_f64() {
+    throw "not implemented";
+}
 
-char32_t BincodeDeserializer::deserialize_char() { throw "not implemented"; }
+inline char32_t BincodeDeserializer::deserialize_char() {
+    throw "not implemented";
+}
 
-bool BincodeDeserializer::deserialize_bool() { return (bool)read_byte(); }
+inline bool BincodeDeserializer::deserialize_bool() {
+    return (bool)read_byte();
+}
 
-uint8_t BincodeDeserializer::deserialize_u8() { return read_byte(); }
+inline uint8_t BincodeDeserializer::deserialize_u8() { return read_byte(); }
 
-uint16_t BincodeDeserializer::deserialize_u16() {
+inline uint16_t BincodeDeserializer::deserialize_u16() {
     uint16_t val = 0;
     val |= (uint16_t)read_byte();
     val |= (uint16_t)read_byte() << 8;
     return val;
 }
 
-uint32_t BincodeDeserializer::deserialize_u32() {
+inline uint32_t BincodeDeserializer::deserialize_u32() {
     uint32_t val = 0;
     val |= (uint32_t)read_byte();
     val |= (uint32_t)read_byte() << 8;
@@ -197,7 +209,7 @@ uint32_t BincodeDeserializer::deserialize_u32() {
     return val;
 }
 
-uint64_t BincodeDeserializer::deserialize_u64() {
+inline uint64_t BincodeDeserializer::deserialize_u64() {
     uint64_t val = 0;
     val |= (uint64_t)read_byte();
     val |= (uint64_t)read_byte() << 8;
@@ -210,44 +222,44 @@ uint64_t BincodeDeserializer::deserialize_u64() {
     return val;
 }
 
-uint128_t BincodeDeserializer::deserialize_u128() {
+inline uint128_t BincodeDeserializer::deserialize_u128() {
     uint128_t result;
     result.low = deserialize_u64();
     result.high = deserialize_u64();
     return result;
 }
 
-int8_t BincodeDeserializer::deserialize_i8() {
+inline int8_t BincodeDeserializer::deserialize_i8() {
     return (int8_t)deserialize_u8();
 }
 
-int16_t BincodeDeserializer::deserialize_i16() {
+inline int16_t BincodeDeserializer::deserialize_i16() {
     return (int16_t)deserialize_u16();
 }
 
-int32_t BincodeDeserializer::deserialize_i32() {
+inline int32_t BincodeDeserializer::deserialize_i32() {
     return (int32_t)deserialize_u32();
 }
 
-int64_t BincodeDeserializer::deserialize_i64() {
+inline int64_t BincodeDeserializer::deserialize_i64() {
     return (int64_t)deserialize_u64();
 }
 
-int128_t BincodeDeserializer::deserialize_i128() {
+inline int128_t BincodeDeserializer::deserialize_i128() {
     int128_t result;
     result.low = deserialize_u64();
     result.high = deserialize_i64();
     return result;
 }
 
-size_t BincodeDeserializer::deserialize_len() {
+inline size_t BincodeDeserializer::deserialize_len() {
     return (size_t)deserialize_u64();
 }
 
-size_t BincodeDeserializer::deserialize_variant_index() {
+inline size_t BincodeDeserializer::deserialize_variant_index() {
     return (size_t)deserialize_u32();
 }
 
-bool BincodeDeserializer::deserialize_option_tag() {
+inline bool BincodeDeserializer::deserialize_option_tag() {
     return deserialize_bool();
 }
