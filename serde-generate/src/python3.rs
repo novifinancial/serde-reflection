92559e78cda55b2d69aed053af954d90e021af81
Mathieu Baudet <mathieubaudet@calibra.com>
2020-08-03 12:46:48 -0700

    [serde generate] python: add PythonEmitter

--- serde-generate/src/python3.rs
+++ serde-generate/src/python3.rs
@@ -10,11 +10,7 @@ use std::path::PathBuf;
 /// * The packages `dataclasses` and `typing` are assumed to be available.
 /// * The module `serde_types` is assumed to be available.
 pub fn output(out: &mut dyn Write, registry: &Registry) -> Result<()> {
-    output_preamble(out, None)?;
-    for (name, format) in registry {
-        output_container(out, name, format)?;
-    }
-    Ok(())
+    output_with_optional_serde_package(out, registry, None)
 }
 
 fn output_with_optional_serde_package(
@@ -22,168 +18,200 @@ fn output_with_optional_serde_package(
     registry: &Registry,
     serde_package_name: Option<String>,
 ) -> Result<()> {
-    output_preamble(out, serde_package_name)?;
+    let mut emitter = PythonEmitter {
+        out,
+        indentation: 0,
+        serde_package_name,
+    };
+    emitter.output_preamble()?;
     for (name, format) in registry {
-        output_container(out, name, format)?;
+        emitter.output_container(name, format)?;
     }
     Ok(())
 }
 
-fn output_preamble(out: &mut dyn Write, serde_package_name: Option<String>) -> Result<()> {
-    writeln!(
-        out,
-        r#"from dataclasses import dataclass
+struct PythonEmitter<T> {
+    out: T,
+    indentation: usize,
+    serde_package_name: Option<String>,
+}
+
+impl<T> PythonEmitter<T>
+where
+    T: Write,
+{
+    fn output_preamble(&mut self) -> Result<()> {
+        assert_eq!(self.indentation, 0);
+        writeln!(
+            self.out,
+            r#"from dataclasses import dataclass
 import typing
 {}import serde_types as st"#,
-        match serde_package_name {
-            None => "".to_string(),
-            Some(name) => format!("from {} ", name),
+            match &self.serde_package_name {
+                None => "".to_string(),
+                Some(name) => format!("from {} ", name),
+            }
+        )
+    }
+
+    fn quote_type(format: &Format) -> String {
+        use Format::*;
+        match format {
+            TypeName(x) => format!("\"{}\"", x), // Need quotes because of circular dependencies.
+            Unit => "st.unit".into(),
+            Bool => "st.bool".into(),
+            I8 => "st.int8".into(),
+            I16 => "st.int16".into(),
+            I32 => "st.int32".into(),
+            I64 => "st.int64".into(),
+            I128 => "st.int128".into(),
+            U8 => "st.uint8".into(),
+            U16 => "st.uint16".into(),
+            U32 => "st.uint32".into(),
+            U64 => "st.uint64".into(),
+            U128 => "st.uint128".into(),
+            F32 => "st.float32".into(),
+            F64 => "st.float64".into(),
+            Char => "st.char".into(),
+            Str => "str".into(),
+            Bytes => "bytes".into(),
+
+            Option(format) => format!("typing.Optional[{}]", Self::quote_type(format)),
+            Seq(format) => format!("typing.Sequence[{}]", Self::quote_type(format)),
+            Map { key, value } => format!(
+                "typing.Dict[{}, {}]",
+                Self::quote_type(key),
+                Self::quote_type(value)
+            ),
+            Tuple(formats) => format!("typing.Tuple[{}]", Self::quote_types(formats)),
+            TupleArray { content, size } => format!(
+                "typing.Tuple[{}]",
+                Self::quote_types(&vec![content.as_ref().clone(); *size])
+            ), // Sadly, there are no fixed-size arrays in python.
+
+            Variable(_) => panic!("unexpected value"),
         }
-    )
-}
-
-fn quote_type(format: &Format) -> String {
-    use Format::*;
-    match format {
-        TypeName(x) => format!("\"{}\"", x), // Need quotes because of circular dependencies.
-        Unit => "st.unit".into(),
-        Bool => "st.bool".into(),
-        I8 => "st.int8".into(),
-        I16 => "st.int16".into(),
-        I32 => "st.int32".into(),
-        I64 => "st.int64".into(),
-        I128 => "st.int128".into(),
-        U8 => "st.uint8".into(),
-        U16 => "st.uint16".into(),
-        U32 => "st.uint32".into(),
-        U64 => "st.uint64".into(),
-        U128 => "st.uint128".into(),
-        F32 => "st.float32".into(),
-        F64 => "st.float64".into(),
-        Char => "st.char".into(),
-        Str => "str".into(),
-        Bytes => "bytes".into(),
-
-        Option(format) => format!("typing.Optional[{}]", quote_type(format)),
-        Seq(format) => format!("typing.Sequence[{}]", quote_type(format)),
-        Map { key, value } => format!("typing.Dict[{}, {}]", quote_type(key), quote_type(value)),
-        Tuple(formats) => format!("typing.Tuple[{}]", quote_types(formats)),
-        TupleArray { content, size } => format!(
-            "typing.Tuple[{}]",
-            quote_types(&vec![content.as_ref().clone(); *size])
-        ), // Sadly, there are no fixed-size arrays in python.
-
-        Variable(_) => panic!("unexpected value"),
     }
-}
 
-fn quote_types(formats: &[Format]) -> String {
-    formats
-        .iter()
-        .map(quote_type)
-        .collect::<Vec<_>>()
-        .join(", ")
-}
-
-fn output_fields(out: &mut dyn Write, indentation: usize, fields: &[Named<Format>]) -> Result<()> {
-    let tab = " ".repeat(indentation);
-    for field in fields {
-        writeln!(out, "{}{}: {}", tab, field.name, quote_type(&field.value))?;
+    fn quote_types(formats: &[Format]) -> String {
+        formats
+            .iter()
+            .map(Self::quote_type)
+            .collect::<Vec<_>>()
+            .join(", ")
     }
-    Ok(())
-}
 
-fn output_variant(
-    out: &mut dyn Write,
-    base: &str,
-    name: &str,
-    index: u32,
-    variant: &VariantFormat,
-) -> Result<()> {
-    use VariantFormat::*;
-    match variant {
-        Unit => writeln!(
-            out,
-            "\n@dataclass\nclass {}__{}({}):\n    INDEX = {}\n",
-            base, name, base, index,
-        ),
-        NewType(format) => writeln!(
-            out,
-            "\n@dataclass\nclass {}__{}({}):\n    INDEX = {}\n    value: {}\n",
-            base,
-            name,
-            base,
-            index,
-            quote_type(format)
-        ),
-        Tuple(formats) => writeln!(
-            out,
-            "\n@dataclass\nclass {}__{}({}):\n    INDEX = {}\n    value: typing.Tuple[{}]\n",
-            base,
-            name,
-            base,
-            index,
-            quote_types(formats)
-        ),
-        Struct(fields) => {
+    fn output_fields(&mut self, fields: &[Named<Format>]) -> Result<()> {
+        self.indentation += 1;
+        let tab = "    ".repeat(self.indentation);
+        for field in fields {
             writeln!(
-                out,
-                "\n@dataclass\nclass {}__{}({}):\n    INDEX = {}",
-                base, name, base, index
+                self.out,
+                "{}{}: {}",
+                tab,
+                field.name,
+                Self::quote_type(&field.value)
             )?;
-            output_fields(out, 4, fields)?;
-            writeln!(out)
         }
-        Variable(_) => panic!("incorrect value"),
+        self.indentation -= 1;
+        Ok(())
     }
-}
 
-fn output_variants(
-    out: &mut dyn Write,
-    base: &str,
-    variants: &BTreeMap<u32, Named<VariantFormat>>,
-) -> Result<()> {
-    for (index, variant) in variants {
-        output_variant(out, base, &variant.name, *index, &variant.value)?;
-    }
-    Ok(())
-}
-
-fn output_container(out: &mut dyn Write, name: &str, format: &ContainerFormat) -> Result<()> {
-    use ContainerFormat::*;
-    match format {
-        UnitStruct => writeln!(out, "\n@dataclass\nclass {}:\n    pass\n", name),
-        NewTypeStruct(format) => writeln!(
-            out,
-            "\n@dataclass\nclass {}:\n    value: {}\n",
-            name,
-            quote_type(format)
-        ),
-        TupleStruct(formats) => writeln!(
-            out,
-            "\n@dataclass\nclass {}:\n    value: typing.Tuple[{}]\n",
-            name,
-            quote_types(formats)
-        ),
-        Struct(fields) => {
-            writeln!(out, "\n@dataclass\nclass {}:", name)?;
-            output_fields(out, 4, fields)?;
-            writeln!(out)
-        }
-        Enum(variants) => {
-            // Initializing VARIANTS with a temporary value for typechecking purposes.
-            writeln!(out, "\nclass {}:\n    VARIANTS = []\n", name)?;
-            output_variants(out, name, variants)?;
-            writeln!(
-                out,
-                "{}.VARIANTS = [\n{}]\n",
+    fn output_variant(
+        &mut self,
+        base: &str,
+        name: &str,
+        index: u32,
+        variant: &VariantFormat,
+    ) -> Result<()> {
+        assert_eq!(self.indentation, 0);
+        use VariantFormat::*;
+        match variant {
+            Unit => writeln!(
+                self.out,
+                "\n@dataclass\nclass {}__{}({}):\n    INDEX = {}\n",
+                base, name, base, index,
+            ),
+            NewType(format) => writeln!(
+                self.out,
+                "\n@dataclass\nclass {}__{}({}):\n    INDEX = {}\n    value: {}\n",
+                base,
                 name,
-                variants
-                    .iter()
-                    .map(|(_, v)| format!("    {}__{},\n", name, v.name))
-                    .collect::<Vec<_>>()
-                    .join("")
-            )
+                base,
+                index,
+                Self::quote_type(format)
+            ),
+            Tuple(formats) => writeln!(
+                self.out,
+                "\n@dataclass\nclass {}__{}({}):\n    INDEX = {}\n    value: typing.Tuple[{}]\n",
+                base,
+                name,
+                base,
+                index,
+                Self::quote_types(formats)
+            ),
+            Struct(fields) => {
+                writeln!(
+                    self.out,
+                    "\n@dataclass\nclass {}__{}({}):\n    INDEX = {}",
+                    base, name, base, index
+                )?;
+                self.output_fields(fields)?;
+                writeln!(self.out)
+            }
+            Variable(_) => panic!("incorrect value"),
+        }
+    }
+
+    fn output_variants(
+        &mut self,
+        base: &str,
+        variants: &BTreeMap<u32, Named<VariantFormat>>,
+    ) -> Result<()> {
+        assert_eq!(self.indentation, 0);
+        for (index, variant) in variants {
+            self.output_variant(base, &variant.name, *index, &variant.value)?;
+        }
+        Ok(())
+    }
+
+    fn output_container(&mut self, name: &str, format: &ContainerFormat) -> Result<()> {
+        assert_eq!(self.indentation, 0);
+        use ContainerFormat::*;
+        match format {
+            UnitStruct => writeln!(self.out, "\n@dataclass\nclass {}:\n    pass\n", name),
+            NewTypeStruct(format) => writeln!(
+                self.out,
+                "\n@dataclass\nclass {}:\n    value: {}\n",
+                name,
+                Self::quote_type(format)
+            ),
+            TupleStruct(formats) => writeln!(
+                self.out,
+                "\n@dataclass\nclass {}:\n    value: typing.Tuple[{}]\n",
+                name,
+                Self::quote_types(formats)
+            ),
+            Struct(fields) => {
+                writeln!(self.out, "\n@dataclass\nclass {}:", name)?;
+                self.output_fields(fields)?;
+                writeln!(self.out)
+            }
+            Enum(variants) => {
+                // Initializing VARIANTS with a temporary value for typechecking purposes.
+                writeln!(self.out, "\nclass {}:\n    VARIANTS = []\n", name)?;
+                self.output_variants(name, variants)?;
+                writeln!(
+                    self.out,
+                    "{}.VARIANTS = [\n{}]\n",
+                    name,
+                    variants
+                        .iter()
+                        .map(|(_, v)| format!("    {}__{},\n", name, v.name))
+                        .collect::<Vec<_>>()
+                        .join("")
+                )
+            }
         }
     }
 }
