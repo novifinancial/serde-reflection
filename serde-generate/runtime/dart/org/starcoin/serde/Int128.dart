299e46fa04f6577217a691114b328e9da39b4eaf
fanngyuan <fanngyuan@gmail.com>
2020-09-22 16:40:22 +0800

    add json fucntion

--- serde-generate/runtime/dart/org/starcoin/serde/Int128.dart
+++ serde-generate/runtime/dart/org/starcoin/serde/Int128.dart
@@ -9,6 +9,19 @@ class Int128 {
     this.low = low;
   }
 
+  Int128 fromBigInt(BigInt num) {
+    high = (num >> 64).toInt();
+    low = (num & BigInt.from(0xFFFFFFFFFFFFFFFF)).toInt();
+    return Int128(high, low);
+  }
+
+  BigInt toBigInt() {
+    final result = BigInt.from(high);
+    result << 64;
+    result + BigInt.from(low);
+    return result;
+  }
+
   @override
   bool operator ==(covariant Int128 other) {
     if (other == null) return false;
@@ -21,4 +34,19 @@ class Int128 {
 
   @override
   int get hashCode => $jf($jc(this.high.hashCode, this.low));
+
+  @override
+  String toString() {
+    return "$high$low";
+  }
+
+  Int128.fromJson(Map<String, dynamic> json) {
+    final num = json['value'] as BigInt;
+    high = (num >> 64).toInt();
+    low = (num & BigInt.from(0xFFFFFFFFFFFFFFFF)).toInt();
+  }
+
+  Map<String, dynamic> toJson() => {
+        "value": this.toBigInt(),
+      };
 }
