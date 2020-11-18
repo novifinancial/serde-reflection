d2438029508b2191059546f1b003beb5338955ba
Mathieu Baudet <mathieubaudet@fb.com>
2020-11-18 12:07:30 -0800

    [serde-generate] Use java 8 syntax to annotate qualified names

--- serde-generate/tests/golang_runtime.rs
+++ serde-generate/tests/golang_runtime.rs
@@ -193,15 +193,16 @@ func main() {{
 		if err != nil {{ panic(fmt.Sprintf("failed to deserialize input: %v", err)) }}
 		output, err := value.{2}Serialize()
 		if err != nil {{ panic(fmt.Sprintf("failed to serialize: %v", err)) }}
-
 		if !cmp.Equal(input, output) {{ panic(fmt.Sprintf("input != output:\n  %v\n  %v", input, output)) }}
 
+		// Test self-equality for the Serde value.
 		{{
 			value2, err := {2}DeserializeSerdeData(input)
 			if err != nil {{ panic(fmt.Sprintf("failed to deserialize input: %v", err)) }}
 			if !cmp.Equal(value, value2) {{ panic("Value should test equal to itself.") }}
 		}}
 
+		// Test simple mutations of the input.
 		for i := 0; i < len(input); i++ {{
 			input2 := make([]byte, len(input))
 			copy(input2, input)
