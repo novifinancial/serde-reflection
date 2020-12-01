4358a064e5973b610390b676f3c6669139af748a
ci-build <ci-build@calibra.com>
2020-12-01 05:46:43 +0000

    [skip ci] documentation update

--- implementors/core/convert/trait.AsRef.js
+++ implementors/core/convert/trait.AsRef.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {};
-implementors["anyhow"] = [{"text":"impl AsRef&lt;dyn Error + 'static + Send + Sync&gt; for Error","synthetic":false,"types":[]},{"text":"impl AsRef&lt;dyn Error + 'static&gt; for Error","synthetic":false,"types":[]}];
+implementors["anyhow"] = [{"text":"impl AsRef&lt;dyn Error + 'static + Sync + Send&gt; for Error","synthetic":false,"types":[]},{"text":"impl AsRef&lt;dyn Error + 'static&gt; for Error","synthetic":false,"types":[]}];
 implementors["serde_bytes"] = [{"text":"impl AsRef&lt;[u8]&gt; for Bytes","synthetic":false,"types":[]},{"text":"impl AsRef&lt;[u8]&gt; for ByteBuf","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ serde_generate/csharp/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["CodeGenerator","Main configuration object for code-generation in C#."],["Installer","Installer for generated source files in C#."]]});
