5ac43c4202b41fea39c05715f1608dd3b9b64df5
ci-build <ci-build@calibra.com>
2021-09-15 00:40:13 +0000

    [skip ci] documentation update

--- implementors/core/marker/trait.Sized.js
+++ implementors/core/marker/trait.Sized.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["serde_bytes"] = [{"text":"impl !<a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a> for <a class=\"struct\" href=\"serde_bytes/struct.Bytes.html\" title=\"struct serde_bytes::Bytes\">Bytes</a>","synthetic":true,"types":["serde_bytes::bytes::Bytes"]}];
+implementors["serde_bytes"] = [{"text":"impl !<a class=\"trait\" href=\"https://doc.rust-lang.org/1.55.0/core/marker/trait.Sized.html\" title=\"trait core::marker::Sized\">Sized</a> for <a class=\"struct\" href=\"serde_bytes/struct.Bytes.html\" title=\"struct serde_bytes::Bytes\">Bytes</a>","synthetic":true,"types":["serde_bytes::bytes::Bytes"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
