78acb89943e13e1ff8e04614fae7f3684b58f0e7
ci-build <ci-build@calibra.com>
2021-10-08 19:57:15 +0000

    [skip ci] documentation update

--- serde_generate/typescript/sidebar-items.js
+++ serde_generate/typescript/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["CodeGenerator","Main configuration object for code-generation in TypeScript."],["Installer","Installer for generated source files in TypeScript."]]});
+initSidebarItems({"struct":[["CodeGenerator","Main configuration object for code-generation in TypeScript, powered by the Deno runtime."],["Installer","Installer for generated source files in TypeScript."]]});
