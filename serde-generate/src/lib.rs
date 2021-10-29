7b34d07add2884f9ae3c0d0947f9cc8dbcfc0427
Mathieu Baudet <1105398+matbd@users.noreply.github.com>
2021-10-28 17:21:13 -0700

    Revert "add dart generator"
    This reverts commit 7a6d0690ba9ec701054b276104e1cc323b5f4088.

--- serde-generate/src/lib.rs
+++ serde-generate/src/lib.rs
@@ -140,8 +140,6 @@ pub mod indent;
 pub mod cpp;
 /// Support for code-generation in C#
 pub mod csharp;
-/// Support for code-generation in Dart
-pub mod dart;
 /// Support for code-generation in Go
 pub mod golang;
 /// Support for code-generation in Java
